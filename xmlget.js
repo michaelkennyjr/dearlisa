df3b474e7e061978d7cd8cd060c837c7ba24551e
michaelkennyjr <42083476+michaelkennyjr@users.noreply.github.com>
2019-03-18 18:00:35 -0500

    Update xmlget.js

--- xmlget.js
+++ xmlget.js
@@ -1,3 +1,10 @@
+// Get countdown number from URL and convert to XML index
+var url = window.location.pathname;
+url = url.replace("/dearlisa/", "");
+url = url.replace(".html", "");
+url = url.replace("test", "");
+var daysLeft = 82 - Number(url);
+
 // Make XML request
 var client = new XMLHttpRequest();
 client.onreadystatechange = function() {
@@ -8,16 +15,11 @@ client.onreadystatechange = function() {
 client.open("GET", "pages.xml", true);
 client.send();
 
-// Get countdown number from URL and convert to XML index
-var url = window.location.pathname;
-url = url.replace("/dearlisa/", "");
-url = url.replace(".html", "");
-url = url.replace("test", "");
-var daysLeft = 82 - Number(url);
-
 // Function to fill in HTML using XML response
 function xmlLoad(xml) {
     var xmlDoc = xml.responseXML;
-    var resp = xmlDoc.getElementsByTagName("date")[daysLeft].childNodes[0].nodeValue;
-	  document.getElementById("date").innerHTML = resp;
+    var today = xmlDoc.getElementsByTagName("date")[daysLeft].childNodes[0].nodeValue;
+    
+    document.getElementById("date").innerHTML = today;
+    document.getElementById("count").innerHTML = daysLeft + "DAYS LEFT!";
 }

9f9b2b318fe60868247ec94ca541cc7f6e72e8d2
michaelkennyjr <42083476+michaelkennyjr@users.noreply.github.com>
2019-03-21 15:24:41 -0500

    Update xmlget.js

--- xmlget.js
+++ xmlget.js
@@ -22,7 +22,7 @@ function xmlLoad(xml, cd) {
     
     // Fill in body text with each paragraph
     var insText = "";
-    var todayPage = xmlDoc.getElementsByTagName("text")[daysleft].childNodes;
+    var todayPage = xmlDoc.getElementsByTagName("text")[daysLeft].childNodes;
     var i;
     for (i = 0; i < todayPage.length; i++) {
         insText += todayPage[i].nodeValue + "<br><br>";

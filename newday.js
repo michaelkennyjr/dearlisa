182a72b43cdcc4cd2c33b0f72274929ffdac8251
michaelkennyjr <42083476+michaelkennyjr@users.noreply.github.com>
2019-04-02 15:28:38 -0500

    Create newday.js

---
+++ newday.js
@@ -0,0 +1,34 @@
+// Run functions when button is clicked
+
+function button(cd, op) {
+
+  // Change countdown date depending on which button was pressed
+  switch(op) {
+    // Go to first day (2/15, 113 to go)
+    case 0:
+      cd = 113;
+      break;
+      
+    // Go to previous day
+    case 1:
+      cd--;
+      break;
+      
+    // Go to next day
+    case 2:
+      cd++;
+      break;
+      
+    // Go to today
+    default:
+      var wedDate = new Date(2019, 5, 8);
+      var today = new Date();
+      cd = Math.ceil((wedDate - today) / 1000 / 60 / 60 / 24);
+  }
+  
+  // Run xmlGet to reset page to new day
+  xmlGet(cd);
+  
+  // Return the new countdown number
+  return cd;
+}

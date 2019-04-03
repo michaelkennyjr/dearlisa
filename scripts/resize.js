d49eb92c128576b1f58afb2fe53b37928cc834c3
michaelkennyjr <42083476+michaelkennyjr@users.noreply.github.com>
2019-04-03 13:05:00 -0500

    Update resize.js

--- scripts/resize.js
+++ scripts/resize.js
@@ -1,13 +1,42 @@
 function resize() {
-	  if (window.innerWidth > 1000) {
-		    document.getElementById("scrollbox").style.left = "calc((100% - 900px) / 2)";
-		    document.getElementById("scrollbox").style.right = "calc((100% - 900px) / 2)";
-	  } else {
-		    var imgh = document.getElementById("image").height;
-		    var winw = window.innerWidth;
-		    var bot = imgh + winw * 0.05;
+    if (window.innerWidth > 1000) {
+	    
+	// DESKTOP RESIZING RULES
+        document.getElementById("scrollbox").style.left = "calc((100% - 900px) / 2)";
+        document.getElementById("scrollbox").style.right = "calc((100% - 900px) / 2)";
+	    
+    } else {
+	    
+	// MOBILE RESIZING RULES
+	    
+	// Get aspect ratio of image
+	var pic = document.getElementById("image");
+	var picratio = pic.naturalWidth / pic.naturalHeight;
+	
+	// Get aspect ratio of frame
+	var frame = document.getElementById("frame");
+	var frameratio = frame.width / frame.height;
+	    
+	// If image is wider than frame, stretch horizontally and push to bottom
+	if (picratio > frameratio) {
+            pic.style.width = "100%";
+	    pic.style.height = "";
+	    pic.style.left = "0";
+            pic.style.bottom = "0";
+	// If image is narrower than frame, stretch vertically and keep in middle	
+	} else {
+	    pic.style.height = "100%";
+            pic.style.width = "";
+	    pic.style.left = (frame.width - pic.width) / 2 + "px";
+            pic.style.bottom = "0";
+	}
+	    
+        var imgh = pic.height;
+	var winw = window.innerWidth;
+	var bot = imgh + winw * 0.05;
 
-		    document.getElementById("scrollbox").style.bottom = bot + "px";
-		    document.getElementById("scrollbox").style.left = "5%";
-		    document.getElementById("scrollbox").style.right = "5%";
+	document.getElementById("scrollbox").style.bottom = bot + "px";
+	document.getElementById("scrollbox").style.left = "5%";
+	document.getElementById("scrollbox").style.right = "5%";
     }
+}
